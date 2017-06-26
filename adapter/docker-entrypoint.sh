e504a2e2dbc5a78aac66016026d4270e435b4a3a
zhouyq <zhouyq@goodrain.com>
2017-06-26 15:33:10 +0800

    adapter 的grproxy启动命令修改

--- adapter/docker-entrypoint.sh
+++ adapter/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /etc/goodrain
 wget -q -P /etc/goodrain http://down.goodrain.me/kube-proxy.kubeconfig
-# 启动grproxy
-/grproxy --depend-service=${DEPEND_SERVICE} --v=2 --bind-address=127.0.0.1 \
-  --kubeconfig=/etc/goodrain/kube-proxy.kubeconfig --healthz-port=0 --namespace=${TENANT_ID}
 
+# 启动grproxy
+exec /grproxy --depend-service=${DEPEND_SERVICE} --v=2 --bind-address=127.0.0.1 \
+  --kubeconfig=/etc/goodrain/kube-proxy.kubeconfig --healthz-port=0 --namespace=${TENANT_ID}
