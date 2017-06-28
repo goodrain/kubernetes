c0a581212ff7442798fa0e66b776114b34fe402c
barnett <zengqg@goodrain.com>
2017-06-28 14:32:29 +0800

    adapter 下载路径更改

--- adapter/docker-entrypoint.sh
+++ adapter/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /etc/goodrain
-wget -q -P /etc/goodrain http://down.goodrain.me/kube-proxy.kubeconfig
+wget -q -P /etc/goodrain http://download.goodrain.me/k8s/kube-proxy.kubeconfig
 
 # 启动grproxy
 exec /grproxy --depend-service=${DEPEND_SERVICE} --v=2 --bind-address=127.0.0.1 \
