a22b86b7b609f2840fc6dfa2f8fc3c163d8f7ea4
barnett <zengqg@goodrain.com>
2017-07-17 15:03:29 +0800

    修改adapter容器时挂载配置文件

--- adapter/docker-entrypoint.sh
+++ adapter/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-mkdir /etc/goodrain
-wget -q -P /etc/goodrain http://download.goodrain.me/k8s/kube-proxy.kubeconfig
 
 # 启动grproxy
 exec /grproxy --depend-service=${DEPEND_SERVICE} --v=2 --bind-address=127.0.0.1 \
-  --kubeconfig=/etc/goodrain/kube-proxy.kubeconfig --healthz-port=0 --namespace=${TENANT_ID}
+  --kubeconfig=/etc/kubernetes/kube-proxy.kubeconfig --healthz-port=0 --namespace=${TENANT_ID}
