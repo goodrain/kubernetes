e4afc956532d1392ce026cd50f7511a940f31dce
barnett <zengqg@goodrain.com>
2018-06-14 09:58:08 +0800

    Single instance applications tolerate instance exceptions(Rainbond #83)

--- pkg/proxy/userspace/roundrobin.go
+++ pkg/proxy/userspace/roundrobin.go
@@ -267,6 +267,12 @@ func (lb *LoadBalancerRR) OnEndpointsUpdate(allEndpoints []api.Endpoints) {
 					portsToEndpoints[port.Name] = append(portsToEndpoints[port.Name], hostPortPair{addr.IP, int(port.Port)})
 					// Ignore the protocol field - we'll get that from the Service objects.
 				}
+				if value, ok := svcEndpoints.Annotations["rainbond.com/tolerate-unready-endpoints"]; ok && value == "true" {
+					for i := range ss.NotReadyAddresses {
+						addr := &ss.NotReadyAddresses[i]
+						portsToEndpoints[port.Name] = append(portsToEndpoints[port.Name], hostPortPair{addr.IP, int(port.Port)})
+					}
+				}
 			}
 		}
 
